a762a0ade29f1e491e33a607e9f173ddb1986f7c
ha-nagisa <nagisa18222@icloud.com>
2021-08-17 23:27:39 +0900

    hooksの作成

---
+++ framework/common/cart/use-add-item.ts
@@ -0,0 +1,11 @@
+import { useHook } from "@common/utils/use-hook";
+
+const useAddItem = () => {
+  const hook = useHook((hooks) => {
+    return hooks.cart.useAddItem;
+  });
+
+  return hook.useHook();
+};
+
+export default useAddItem;

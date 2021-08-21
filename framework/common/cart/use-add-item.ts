12d7c4eefb69c6abbe4877e4f01e44722c4b86eb
ha-nagisa <nagisa18222@icloud.com>
2021-08-21 16:36:17 +0900

    カートに追加ボタンの実装

--- framework/common/cart/use-add-item.ts
+++ framework/common/cart/use-add-item.ts
@@ -1,11 +1,11 @@
-import { useHook } from "@common/utils/use-hook";
+import { useHook, useMutationHook } from "@common/utils/use-hook";
 
 const useAddItem = () => {
   const hook = useHook((hooks) => {
     return hooks.cart.useAddItem;
   });
 
-  return hook.useHook();
+  return useMutationHook({ ...hook });
 };
 
 export default useAddItem;
